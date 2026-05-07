cf31247ec8112935c6596d09099b66240c20a054
Shreeraksha U <142918786+Shreeraksha-U@users.noreply.github.com>
2026-05-07 19:59:39 +0530

    Time: 3 ms (78.84%) | Memory: 46.6 MB (39.34%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            int cur_val=val(s.charAt(i));
+            if(i<s.length()-1&&cur_val<val(s.charAt(i+1))) sum-=cur_val;
+            else sum+=cur_val;
+        }
+        return sum;
+    }
+    private int val(char c){
+        switch(c){
+            case 'I':return 1;
+            case 'V' :return 5;
+             case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            default : return 1000;
+        }
+    }
+}
